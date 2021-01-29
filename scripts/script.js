3f94ff1bc31db81c04bd37fc83cbd088f975365e
Ivan Nikitin <iva3nik@yandex.ru>
2021-01-30 00:48:43 +0300

    fix: js has been corrected

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,17 @@ let popup = document.querySelector('.popup');
 let buttonEditName = document.querySelector('.profile__button-edit-name');
 let buttonCloseForm = popup.querySelector('.popup__close-form');
 
+let formElement = popup.querySelector('.popup__edit-profile');
+let nameInput = formElement.querySelector('.popup__item-profile_input_name');
+let jobInput = formElement.querySelector('.popup__item-profile_input_job');
+
+let profileName = document.querySelector('.profile__name');
+let profileText = document.querySelector('.profile__text');
+
 function openEditProfile() {
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileText.textContent;
 }
 
 function closeForm() {
@@ -13,28 +22,20 @@ function closeForm() {
 buttonEditName.addEventListener('click', openEditProfile);
 buttonCloseForm.addEventListener('click', closeForm);
 
-// Находим форму в DOM
-let formElement = popup.querySelector('.popup__edit-profile');
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__item-profile_input_name');
-let jobInput = formElement.querySelector('.popup__item-profile_input_job');
 
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+
+// Обработчик «отправки» формы, хотя пока она никуда отправляться не будет
 function formSubmitHandler (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                          // Так мы можем определить свою логику отправки.
-                          // О том, как это делать, расскажем позже.
 
-    // Выберите элементы, куда должны быть вставлены значения полей
-  let profileName = document.querySelector('.profile__name');
-  let profileText = document.querySelector('.profile__text');
-    // Вставьте новые значения с помощью textContent
   profileName.textContent = nameInput.value;
   profileText.textContent = jobInput.value;
+
+  closeForm();
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
 
