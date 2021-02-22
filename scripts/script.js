99480a2d25bce0f35efd6940e7743232437fbe53
Ivan Nikitin <iva3nik@yandex.ru>
2021-02-22 16:42:25 +0300

    feat: add validation of forms

--- scripts/script.js
+++ scripts/script.js
@@ -139,3 +139,62 @@ function formSubmitHandlerCard (evt) {
 formElement.addEventListener('submit', formSubmitHandler);
 formElementCard.addEventListener('submit', formSubmitHandlerCard);
 
+function showInputError (formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__item-profile_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+function hideInputError (formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__item-profile_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+function isValid (formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+function setEventListeners (formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__item-profile'));
+  const buttonElement = formElement.querySelector('.popup__save');
+  toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', () => {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+function enableValidation () {
+  const formList = Array.from(popups.querySelectorAll('.popup__edit-profile'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+function hasInvalidInput (inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+function toggleButtonState (inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save_inactive');
+  } else {
+    buttonElement.classList.remove('popup__save_inactive');
+  }
+};
+
+enableValidation();
