8f232cde878564c6c7826620be7b0ab7e189bb20
Ivan Nikitin <iva3nik@yandex.ru>
2021-03-16 14:18:03 +0300

    refactor: open and close popupView have been added to Card.js

--- scripts/Card.js
+++ scripts/Card.js
@@ -5,7 +5,20 @@ const popupAbout = popupView.querySelector('.popup__about');
 
 function openPopup(popupElement) {
   popupElement.classList.add('popup_opened');
-}
+  document.addEventListener('keydown', closePopupEscape);
+};
+
+function closePopup(popupElement) {
+  popupElement.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closePopupEscape);
+};
+
+function closePopupEscape(evt) {
+  const popupOpen = document.querySelector('.popup_opened');
+  if (evt.key === 'Escape') {
+    closePopup(popupOpen);
+  }
+};
 
 export class Card {
   constructor(item, selector) {
