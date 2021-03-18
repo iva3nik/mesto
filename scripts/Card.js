a66fffa43ff060fa6d18396e764b9d50a1981056
Ivan Nikitin <iva3nik@yandex.ru>
2021-03-19 00:22:43 +0300

    refactor: _handleDeleteCard and _getTemplate have been added

--- scripts/Card.js
+++ scripts/Card.js
@@ -2,16 +2,21 @@ import { popupView, popupImage, popupAbout, openPopup } from './utils.js';
 
 export class Card {
   constructor(item, selector) {
-    this._cardItem = document
-      .querySelector(selector).content
-      .querySelector('.card').cloneNode(true);
-    this._cardElement = this._cardItem.querySelector('.card__element');
-    this._buttonLike = this._cardItem.querySelector('.card__like');
-    this._buttonDelete = this._cardItem.querySelector('.card__trash');
-
+    this._selector = selector;
     this._link = item.link;
     this._name = item.name;
   };
+
+  _getTemplate() {
+    this._cardTemplate = document
+      .querySelector(this._selector).content
+      .querySelector('.card').cloneNode(true);
+    this._cardElement = this._cardTemplate.querySelector('.card__element');
+    this._buttonLike = this._cardTemplate.querySelector('.card__like');
+    this._buttonDelete = this._cardTemplate.querySelector('.card__trash');
+
+    return this._cardTemplate;
+  }
 
   _like() {
     this._buttonLike.classList.toggle('card__like_active');
@@ -21,18 +26,21 @@ export class Card {
     this._cardItem.remove();
   };
 
+  _handlePreviewPicture() {
+    openPopup(popupView);
+    popupImage.src = this._link;
+    popupImage.alt = this._name;
+    popupAbout.textContent = this._name;
+  };
+
   _setEventListeners() {
     this._buttonLike.addEventListener('click', () => this._like());
     this._buttonDelete.addEventListener('click', () => this._handleDeleteCard());
-    this._cardElement.addEventListener('click', function(evt) {
-      openPopup(popupView);
-      popupImage.src = evt.target.src;
-      popupImage.alt = evt.target.alt;
-      popupAbout.textContent = evt.target.alt;
-    });
+    this._cardElement.addEventListener('click', () => this._handlePreviewPicture());
   };
 
   getCard() {
+    this._cardItem = this._getTemplate();
     this._cardElement.src = this._link;
     this._cardElement.alt = this._name;
     this._cardItem.querySelector('.card__title').textContent = this._name;
