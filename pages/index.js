0a026c0a2bf0598b30281d45527e13dd312325c8
Ivan Nikitin <iva3nik@yandex.ru>
2021-04-03 15:35:20 +0300

    fix: index.js has been moved

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import { popupEditProfile, nameInput, jobInput, popupAddCard,
   popupAddCardForm, buttonEditName, buttonAddCard, validationClass,
-  initialCards } from '../utils/constants.js';
+  initialCards } from '../scripts/utils/constants.js';
 
-import Card from '../components/Card.js';
-import FormValidator from '../components/FormValidator.js';
-import Section from '../components/Section.js';
-import PopupWithImage from '../components/PopupWithImage.js';
-import PopupWithForm from '../components/PopupWithForm.js';
-import UserInfo from '../components/UserInfo.js';
+import Card from '../scripts/components/Card.js';
+import FormValidator from '../scripts/components/FormValidator.js';
+import Section from '../scripts/components/Section.js';
+import PopupWithImage from '../scripts/components/PopupWithImage.js';
+import PopupWithForm from '../scripts/components/PopupWithForm.js';
+import UserInfo from '../scripts/components/UserInfo.js';
 
 
 const popupWithImage = new PopupWithImage('.popup_view');
