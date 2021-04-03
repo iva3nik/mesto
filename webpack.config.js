f944ef0a4bb7e5f65ebe2d2b2ddee6c45b2a1e0f
Ivan Nikitin <iva3nik@yandex.ru>
2021-04-04 01:10:42 +0300

    fix: dist

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,10 @@ module.exports = {
   },
   module: {
     rules: [{
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: '/node_modules/'
-      },
+      test: /\.js$/,
+      use: 'babel-loader',
+      exclude: '/node_modules/'
+    },
       {
         test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
         type: 'asset/resource',
@@ -32,11 +32,11 @@ module.exports = {
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1
+          }
+        },
           'postcss-loader'
         ]
       },
